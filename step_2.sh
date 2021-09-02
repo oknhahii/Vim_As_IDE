877964051af6148018420d8b8ae7d0bee0d120cf
oknhahii <70265188+oknhahii@users.noreply.github.com>
2021-09-02 20:50:15 +0700

    Update step_2.sh

--- step_2.sh
+++ step_2.sh
@@ -1,8 +1,8 @@
-"curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh 
-"bash install_nvm.sh
-"source ~/.bashrc
-"command -v nvm
-"nvm list-remote
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh 
+#bash install_nvm.sh
+#source ~/.bashrc
+#command -v nvm
+#nvm list-remote
 #nvm install node
 #nvm install v16.8.0
 #nvm alias default 16.8.0
