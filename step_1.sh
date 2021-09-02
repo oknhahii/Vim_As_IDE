beff7a791b4823a1816eaedba6d56eff9ce56a1a
oknhahii <70265188+oknhahii@users.noreply.github.com>
2021-09-02 20:41:04 +0700

    Update step_1.sh

--- step_1.sh
+++ step_1.sh
@@ -2,6 +2,6 @@
 sudo apt install git
 sudo apt-get -y install cmake
 sudo apt-get install python-dev python-pip python3-dev python3-pip
-sudo snap install nodejs --classic 
+sudo snap install node --classic 
 
 
