e098f31fc6e738fc241e616a9ad7453e13f67087
oknhahii <70265188+oknhahii@users.noreply.github.com>
2021-09-03 11:01:51 +0700

    Update step_2.sh

--- step_2.sh
+++ step_2.sh
@@ -1,12 +1,12 @@
 cd
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-#source ~/.bashrc
-#command -v nvm
-#nvm list-remote
-#nvm install node
-#nvm install v16.8.0
-#nvm alias default 16.8.0
-#nvm install lts/fermium
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+command -v nvm
+nvm list-remote
+nvm install node
+nvm install v16.8.0
+nvm alias default 16.8.0
+nvm install lts/fermium
 sudo apt remove cmdtest
 sudo npm install --global yarn
 
